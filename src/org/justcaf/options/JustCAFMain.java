2cb27d87f5d68e60750b3e11ac1e71f25e223b9d
rautamak <13891897+rautamak@users.noreply.github.com>
2019-12-31 16:35:37 +0200

    JustCAFOption: refactor ClockFaceController to the Preference activity

--- src/org/justcaf/options/JustCAFMain.java
+++ src/org/justcaf/options/JustCAFMain.java
@@ -23,22 +23,57 @@ import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.Surface;
+import android.content.Context;
+import android.provider.Settings;
+import android.provider.Settings.Secure;
+import android.util.Log;
+
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+
 import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class JustCAFMain extends SettingsPreferenceFragment {
+public class JustCAFMain extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+        
+    private Context mContext;
+    private ListPreference mClockFace;
+    private static final String TAG = JustCAFMain.class.getSimpleName();
+    
+    private static final String CLOCK_FACE = "android.theme.customization.clock_Face";
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.main);
+        
+        final PreferenceScreen ps = getPreferenceScreen();
+        mClockFace = (ListPreference) ps.findPreference(PREFERENCE_TEMP_UNIT);
+        mClockFace.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.JUSTCAF_OPTIONS;
+    }
+    
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        switch (preference) {
+            case (mClockFace):
+                String newFace = (String) newValue;
+
+                Log.i(TAG, "onPreferenceChange");
+                Log.i(TAG, newFace);
+                Settings.Secure.putString(mContext.getContentResolver(), KEY_CLOCK_FACES, newFace);
+                break;
+                
+        }
+        return true;
     }
 }
